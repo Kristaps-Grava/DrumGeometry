28935ef6dbebbcf883a0240dcbb5f8232e622d8b
Kristaps <kristap.grava@gmail.com>
2025-11-26 01:39:13 +0100

    Adding comments

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@
 
 from core.simulation import Simulation
 
-
+# k and b are values determining the shape of the spiral. for example k=0 means
+# that radius is constant. k=0.01 would mean that for every radian the radius decreases by 1 cm
 sim = Simulation(k=0, b=0.07)
 
 final_angular_velocity = sim.run()
